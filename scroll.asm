bb6e221b92507811aa198a5194057ceb08c44098
Csabo <csabo@rogers.com>
2016-07-05 17:33:54 -0400

    Scroll is done (all letters)

--- scroll.asm
+++ scroll.asm
@@ -126,17 +126,19 @@ no_new_char
 	
 	CHARSET 'A','Z',$01
 	CHARSET '.',$1B
+	CHARSET '!',$1C
 	CHARSET 'a','z',$21
 	
 scroll_text
-	byt "kevim jaj is ..."
+	;byt "z"
+	byt "got your balls"
 	byt $FE,$61
-	byt " part blart mart cart"
+	byt " get ready to rock now! "
 	byt $FE,$57
 	byt " greetings to"
-	byt "                                    ..."
+	byt " All the Plussy people "
 	byt $FE,$41
-	byt "                                    ..."
+	byt " and everyone at Arok"
 	byt $FE,$01 ; scroll over
 	byt " "
 	
@@ -145,7 +147,7 @@ scroll_text
 char_widths
 	byt 2 ; null
 	byt 3,3,3,3,3,3,3,3
-	byt 1,3,3,1,3,3,3,3
+	byt 1,3,3,3,3,3,3,3
 	byt 3,3,3,3,3,3,3,3
 	byt 3,3
 	byt 1,1,1,1,1
@@ -185,7 +187,7 @@ char_ptrs
 	adr char_upper_y
 	adr char_upper_z
 	adr char_period
-	adr char_space
+	adr char_exclamation
 	adr char_space
 	adr char_space
 	adr char_space
@@ -201,7 +203,7 @@ char_ptrs
 	adr char_lower_i
 	adr char_lower_j
 	adr char_lower_k
-	adr char_lower_l
+	adr char_upper_i
 	adr char_lower_m
 	adr char_lower_n
 	adr char_lower_o
@@ -221,37 +223,111 @@ char_space
 	byt 0,0,0,0,0
 	byt 0,0,0,0,0
 char_upper_a
+	byt $0F,$12,$07,$03,$04
+	byt $10,$00,$08,$00,$00
+	byt $11,$13,$0a,$03,$04
 char_upper_b
+	byt $0B,$03,$07,$03,$0D
+	byt $24,$00,$44,$00,$26
+	byt $11,$33,$45,$33,$18
 char_upper_c
+	byt $0F,$12,$03,$14,$16
+	byt $10,$00,$00,$00,$17
+	byt $11,$19,$00,$1B,$18
 char_upper_d
+	byt $0B,$03,$03,$03,$0D
+	byt $24,$00,$00,$00,$26
+	byt $11,$13,$03,$15,$18
 char_upper_e
+	byt $0B,$03,$07,$03,$0D
+	byt $08,$00,$09,$00,$08
+	byt $09,$00,$00,$00,$09
 char_upper_f
+	byt $0B,$03,$07,$03,$04
+	byt $08,$00,$09,$00,$00
+	byt $09,$00,$00,$00,$00
 char_upper_g
+	byt $0F,$12,$03,$14,$16
+	byt $10,$00,$06,$00,$17
+	byt $11,$19,$0C,$15,$18
 char_upper_h
 	byt $01,$03,$07,$03,$04
 	byt $00,$00,$08,$00,$00
 	byt $01,$03,$0A,$03,$04
 char_upper_i
+	byt $01,$03,$03,$03,$04
 char_upper_j
+	byt $00,$00,$00,$1C,$16
+	byt $00,$00,$00,$00,$17
+	byt $01,$03,$03,$15,$18
 char_upper_k
+	byt $01,$03,$07,$03,$04
+	byt $00,$00,$44,$00,$00
+	byt $01,$15,$45,$13,$04
 char_upper_l
+	byt $01,$03,$03,$03,$0D
+	byt $00,$00,$00,$00,$08
+	byt $00,$00,$00,$00,$09
 char_upper_m
+	byt $0B,$03,$03,$03,$04
+	byt $02,$03,$03,$03,$04
+	byt $11,$13,$03,$03,$04
 char_upper_n
+	byt $0B,$03,$03,$03,$04
+	byt $24,$00,$00,$00,$00
+	byt $11,$13,$03,$03,$04
 char_upper_o
+	byt $0F,$12,$03,$14,$16
+	byt $10,$00,$00,$00,$17
+	byt $11,$13,$03,$15,$18
 char_upper_p
+	byt $0B,$03,$07,$03,$04
+	byt $24,$00,$26,$00,$00
+	byt $11,$33,$18,$00,$00
 char_upper_q
+	byt $0F,$12,$03,$14,$16
+	byt $10,$00,$00,$00,$27
+	byt $11,$13,$03,$03,$0E
 char_upper_r
+	byt $0B,$03,$07,$03,$04
+	byt $24,$00,$44,$00,$00
+	byt $11,$33,$45,$13,$04
 char_upper_s
+	byt $0F,$34,$16,$1C,$16
+	byt $10,$00,$48,$00,$17
+	byt $11,$19,$11,$33,$18
 char_upper_t
+	byt $06,$00,$00,$00,$00
+	byt $02,$03,$03,$03,$04
+	byt $09,$00,$00,$00,$00
 char_upper_u
+	byt $01,$03,$03,$14,$16
+	byt $00,$00,$00,$00,$17
+	byt $01,$03,$03,$15,$18
 char_upper_v
+	byt $01,$03,$03,$3C,$3F
+	byt $00,$00,$00,$3D,$40
+	byt $01,$03,$03,$3E,$41
 char_upper_w
+	byt $01,$03,$03,$03,$16
+	byt $01,$03,$03,$03,$05
+	byt $01,$03,$03,$03,$18
 char_upper_x
+	byt $01,$14,$46,$12,$04
+	byt $00,$00,$47,$00,$00
+	byt $01,$15,$45,$13,$04
 char_upper_y
+	byt $01,$14,$16,$00,$06
+	byt $00,$00,$27,$00,$08
+	byt $01,$03,$0A,$03,$18
 char_upper_z
-	byt $00,$00,$00,$00,$00
+	byt $06,$00,$0F,$12,$0D
+	byt $08,$00,$25,$00,$08
+	byt $0C,$03,$18,$00,$09
 char_period
 	byt $00,$00,$00,$00,$23
+char_exclamation
+	byt $01,$03,$03,$04,$23
 char_lower_a
 	byt $00,$0F,$28,$2A,$16
 	byt $00,$10,$1E,$2B,$08
@@ -292,8 +368,6 @@ char_lower_k
 	byt $01,$03,$3A,$3B,$04
 	byt $00,$00,$39,$31,$00
 	byt $00,$01,$35,$36,$04
-char_lower_l
-	byt $01,$03,$03,$03,$04
 char_lower_m
 	byt $00,$0B,$03,$03,$04
 	byt $00,$02,$03,$03,$04
@@ -311,6 +385,9 @@ char_lower_p
 	byt $00,$24,$00,$26,$00
 	byt $00,$11,$33,$18,$00
 char_lower_q
+	byt $00,$0F,$34,$16,$00
+	byt $00,$25,$00,$27,$00
+	byt $00,$0C,$03,$0A,$04
 char_lower_r
 	byt $00,$0B,$03,$03,$04
 	byt $00,$24,$00,$00,$00
@@ -327,12 +404,24 @@ char_lower_u
 	byt $00,$00,$00,$00,$17
 	byt $00,$01,$03,$15,$18
 char_lower_v
-	byt $00,$01,$03,$14,$16
-	byt $00,$00,$00,$00,$17
-	byt $00,$01,$03,$15,$18
+	byt $00,$01,$03,$3C,$3F
+	byt $00,$00,$00,$3D,$40
+	byt $00,$01,$03,$3E,$41
 char_lower_w
+	byt $00,$01,$03,$03,$16
+	byt $00,$01,$03,$03,$05
+	byt $00,$01,$03,$03,$18
 char_lower_x
+	byt $00,$01,$37,$38,$04
+	byt $00,$00,$42,$43,$00
+	byt $00,$01,$35,$36,$04
 char_lower_y
+	byt $00,$01,$14,$16,$06
+	byt $00,$00,$00,$27,$08
+	byt $00,$01,$03,$0A,$18
 char_lower_z
-	byt 0,0,0,0,0
+	byt $00,$06,$0F,$12,$0D
+	byt $00,$08,$25,$00,$08
+	byt $00,$0C,$18,$00,$09
+	
 	
